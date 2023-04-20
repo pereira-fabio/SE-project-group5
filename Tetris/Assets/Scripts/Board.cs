acaa78e07f4dbb8be55c18728e08813a604c8e4e
pereira-fabio <fabio.pereira.002@student.uni.lu>
2023-04-20 23:13:09 +0200

    added the form of the pieces

---
+++ Tetris/Assets/Scripts/Board.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class Board : MonoBehaviour
+{
+    public TetrominoData[] tetrominos;
+    public Tilemap tilemap { get; private set; }
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        
+        for(int i = 0; i < this.tetrominos.Length; i++){
+            this.tetrominos[i].Initialize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, this.tetrominos.Length);
+        TetrominoData data = this.tetrominos[random];
+    }
+
+    public void Set()
+    {
+        
+    }
+}
+
