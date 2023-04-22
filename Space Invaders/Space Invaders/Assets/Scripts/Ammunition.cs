3044ad2d30491613b27448a6a0d9eac32294b630
carvalho-pereira-diogo <71407360+carvalho-pereira-diogo@users.noreply.github.com>
2023-04-22 18:28:46 +0200

    Implementation of plane shooting ammunition
    Implementation of plane shooting ammunition and also ammunition having collisions with other elements

---
+++ Space Invaders/Space Invaders/Assets/Scripts/Ammunition.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammunition : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move the object up
+        transform.Translate(Vector2.up * Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        // check if the object that collided with this object has the tag "Enemy"
+        if(other.gameObject.tag == "Enemy") {
+            // if it does, destroy both objects
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+
+        // check if the object that collided with this object has the tag "Limit"
+        if(other.gameObject.tag == "Limit") {
+            // if it does, destroy this object
+            Destroy(gameObject);
+        }
+    }
+}
