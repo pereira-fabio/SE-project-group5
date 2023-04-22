754a2f85c9b2c835a031e61ff63b28525e72a1d7
carvalho-pereira-diogo <71407360+carvalho-pereira-diogo@users.noreply.github.com>
2023-04-22 17:34:15 +0200

    Giving movement to SchoolMaterial
    Doing some limits in the screen for the monsters and also adding the correct movement for them

---
+++ Space Invaders/Space Invaders/Assets/SchoolMaterialMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SchoolMaterialMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move the object to the right
+        transform.Translate(Vector2.right * Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        // check if the object that collided with this object has the tag "Limit"
+        if (other.gameObject.tag == "Limit") {
+            //Change the position to go down
+            transform.position = new Vector3(transform.position.x, transform.position.y -1, transform.position.z);
+            // if it does, change the direction of the movement
+            speed = -speed;
+        }
+    }
+}
