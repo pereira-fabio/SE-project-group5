cf7dd3eb176804f997429eda6ea9c83d6b09dafc
Piotr Wojsznis <piotr@MacBook-Pro-Piotr.local>
2023-05-22 19:59:56 +0200

    Added core game mechanics

---
+++ Main Game/Assets/Scripts/PCController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PCController : MonoBehaviour, Interactable
+{
+    public void Interact(){
+        Debug.Log("Interacting with PC");
+    }
+}
