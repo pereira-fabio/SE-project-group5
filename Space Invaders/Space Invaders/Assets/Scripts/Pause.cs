8119268773a05b7d649a2c38d1118fc853ec0060
carvalho-pereira-diogo <71407360+carvalho-pereira-diogo@users.noreply.github.com>
2023-05-23 22:26:31 +0200

    Revert "Introduction of PauseMenu"
    This reverts commit 30868794ad6a8c7e7ad17dd6ed1aaca7721de844.

--- Space Invaders/Space Invaders/Assets/Scripts/Pause.cs
+++ Space Invaders/Space Invaders/Assets/Scripts/Pause.cs
@@ -1,14 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
-    public static bool isPaused = false;
+    public bool isPaused = false;
     public GameObject pauseMenu;
-    //Create variable for PLane 
-    public GameObject plane;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +16,10 @@ public class Pause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape)){
-            // if the game is paused
-            if(isPaused){
-                // resume the game
+        if(Input.GetButtonDown("Cancel")) {
+            if(isPaused) {
                 ResumeGame();
             } else {
-                // pause the game
                 PauseGame();
             }
         }
@@ -36,34 +30,16 @@ public class Pause : MonoBehaviour
         Time.timeScale = 0;
         // show the pause menu
         pauseMenu.SetActive(true);
-
-        //Pause script of variable plane
-        plane.GetComponent<PlaneAmmunition>().enabled = false;
-
         // set the isPaused variable to true
         isPaused = true;
     }
-
-
 
     public void ResumeGame(){
         // resume the game
         Time.timeScale = 1;
         // hide the pause menu
         pauseMenu.SetActive(false);
-
-        //Activate the PlaneAmmunition Script of Plane
-        plane.GetComponent<PlaneAmmunition>().enabled = true;
-
         // set the isPaused variable to false
         isPaused = false;
-    }
-
-    public void LoadMenu(){
-        SceneManager.LoadScene("Main Menu");
-    }
-    
-    public void QuitGame(){
-        Application.Quit();
     }
 }
