fff1810c9169d696f95682b7ed299da6edf0e5e0
carvalho-pereira-diogo <71407360+carvalho-pereira-diogo@users.noreply.github.com>
2023-05-25 17:39:39 +0200

    Resume Panel Changes
    Resume Panel Changes

--- Main Game/Assets/Scripts/Space Invaders Scripts/Pause.cs
+++ Main Game/Assets/Scripts/Space Invaders Scripts/Pause.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
     public bool isPaused = false;
     public GameObject pauseMenu;
+    //Get plane object
+    public GameObject plane;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,8 @@ public class Pause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Cancel")) {
+        //Check for ESC key press
+        if(Input.GetKeyDown(KeyCode.Escape)) {
             if(isPaused) {
                 ResumeGame();
             } else {
@@ -30,6 +34,8 @@ public class Pause : MonoBehaviour
         Time.timeScale = 0;
         // show the pause menu
         pauseMenu.SetActive(true);
+        //Desactivate plane script "Plane Ammuntion" inside plane
+        plane.GetComponent<PlaneAmmunition>().enabled = false;
         // set the isPaused variable to true
         isPaused = true;
     }
@@ -39,7 +45,18 @@ public class Pause : MonoBehaviour
         Time.timeScale = 1;
         // hide the pause menu
         pauseMenu.SetActive(false);
+        //Desactivate plane script "Plane Ammuntion" inside plane
+        plane.GetComponent<PlaneAmmunition>().enabled = true;
         // set the isPaused variable to false
         isPaused = false;
+    }
+
+    public void MainMenu(){
+        // resume the game
+        Time.timeScale = 1;
+        // set the isPaused variable to false
+        isPaused = false;
+        // load the main menu scene
+        SceneManager.LoadScene(4);
     }
 }
