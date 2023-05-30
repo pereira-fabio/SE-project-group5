0567572bf9c04c0eab3702692e025e881a6c0426
pereira-fabio <fabio.pereira.002@student.uni.lu>
2023-05-30 22:32:02 +0200

    small explaination to each game, how to move etc

--- Main Game/Assets/Scripts/PCController.cs
+++ Main Game/Assets/Scripts/PCController.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class PCController : MonoBehaviour, Interactable
 {
+    [SerializeField] Dialog dialog;
+
     public void Interact(){
         Debug.Log("Interacting with PC");
-        SceneManager.LoadScene(1);
+        StartDialog();
+        
+        Invoke("LoadScene", 3f);
+    }
+
+    public void StartDialog(){
+        StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
+    }
+
+    public void LoadScene(){
+        SceneManager.LoadScene(1); //quiz
     }
 }
--- Main Game/Assets/Scripts/PCController1.cs
+++ Main Game/Assets/Scripts/PCController1.cs
@@ -5,8 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class PCController1 : MonoBehaviour, Interactable
 {
+    [SerializeField] Dialog dialog;
+
     public void Interact(){
         Debug.Log("Interacting with PC");
-        SceneManager.LoadScene(2);
+        StartDialog();
+        
+        Invoke("LoadScene", 3f);
+    }
+
+    public void StartDialog(){
+        StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
+    }
+
+    public void LoadScene(){
+        SceneManager.LoadScene(2); //Tetris
     }
 }
